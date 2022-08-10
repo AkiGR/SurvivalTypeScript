8f85998280f2a820d1272f3736a8a433373f58fb
hanauchiakiya <hanauchi.aki@gmail.com>
2022-08-10 12:52:13 +0900

    Create index.ts
    アロー関数

---
+++ 関数/アロー関数 (arrow function)/index.ts
@@ -0,0 +1,15 @@
+( num ) => {
+  // 処理
+};
+
+const increment = (n) => {
+  return n + 1;
+};
+
+// 引数がないアロー関数
+const getOne = () => {
+  return 1;
+};
+
+// アロー関数の型注釈
+const increment2 = ( num : number ) : number => num + 1;
